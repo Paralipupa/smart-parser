1c395e8f6d060acd5ef76291f6792ae4d105e1b8
paralipupa <a917kk@mail.ru>
2022-08-21 22:52:39 +0300

    добавлены константы

---
+++ module/settings.py
@@ -0,0 +1,20 @@
+import logging
+
+PATH_LOG = 'logs'
+PATH_OUTPUT = 'output'
+PATH_CONFIG = 'config'
+PATH_TMP = 'tmp'
+
+POS_INDEX_VALUE = 0
+POS_INDEX_IS_NEGATIVE = 1
+
+POS_NUMERIC_VALUE = 0
+POS_NUMERIC_IS_ABSOLUTE = 1
+POS_NUMERIC_IS_NEGATIVE = 2
+
+POS_VALUE = 0
+POS_PAGE_VALUE = 0
+POS_PAGE_IS_FIX = 1
+
+db_logger = logging.getLogger('parser')
+
