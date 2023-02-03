77126c9351b66875a01db25ad376de677d9035d3
paralipupa <a917kk@mail.ru>
2023-02-03 18:05:56 +0300

    исправлен период для bank_accounts. Тест 414

--- module/union.py
+++ module/union.py
@@ -28,14 +28,16 @@ class UnionData:
         if files:
             data = dict()
             del_files = list()
+            period : list = [datetime.now().strftime('%m%Y')]
             for file in files:
                 for fn in DOCUMENTS.split():
                     inn: list = re.findall(
                         '^[0-9]{8,10}(?=_)', file, re.IGNORECASE)
                     name: list = re.findall(
                         '(?<=[0-9]{1}_)'+fn+'(?=\.json)', file, re.IGNORECASE)
-                    period: list = re.findall(
-                        '(?<=[0-9]{1}_)[0-9]{2}[0-9]{4}(?=_)', file, re.IGNORECASE)
+                    if not re.search('bank',file):
+                        period = re.findall(
+                            '(?<=[0-9]{1}_)[0-9]{2}[0-9]{4}(?=_)', file, re.IGNORECASE)
                     if inn and name and period:
                         del_files.append(file)
                         data.setdefault(inn[0], dict())
