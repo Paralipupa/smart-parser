ebf559e4a691a68689ce91b8cb2e934d6c41dd12
chip <a917kk@mai.ru>
2023-10-18 01:04:16 +0300

    Добавлена пени в pp_charges config93

--- preliminary/columns.py
+++ preliminary/columns.py
@@ -88,11 +88,11 @@ def parsing_lines(file, lines: list, ldict: dict, lparam: dict, names: list, pat
                 else:
                     is_duplicate = True
         if not ldict.get("service"):
-            if col_begin > 0 and lparam.get("main_border_column_left"):
+            if idx_col == len(lines)-1 and  col_begin > 0 and lparam.get("main_border_column_left"):
                 file.write(
                     f'border_column_left={lparam.get("main_border_column_left", ["2"])[0]}\n'
                 )
-            if col_begin > 0 and lparam.get("main_border_column_right"):
+            if idx_col == len(lines)-1 and col_begin > 0 and lparam.get("main_border_column_right"):
                 file.write(
                     f'border_column_right={lparam.get("main_border_column_right", ["4"])[0]}\n'
                 )
